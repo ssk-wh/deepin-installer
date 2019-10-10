56bd23bb1f09790b01fdbd6b83dd6716c2c93b73
justforlxz <justforlxz@gmail.com>
2019-10-10 13:09:51 +0800

    chore: chage apt arguments

--- hooks/basic_utils.sh
+++ hooks/basic_utils.sh
@@ -129,5 +129,5 @@ is_arm64() {
 }
 
 install_package() {
-  env DEBIAN_FRONTEND=noninteractive apt-get -y --no-install-recommends --allow-unauthenticated -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install $1
+  DEBIAN_FRONTEND="noninteractive" apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --no-install-recommends --allow-unauthenticated install $1
 }
