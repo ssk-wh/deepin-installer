25400db841f78f2307845ef9179adecb0e4e81f8
chenxiong <chenxiong_cm@deepin.com>
2019-11-19 15:14:27 +0800

    fix: refactor class InstallProgressFrame
    refactor class InstallProgressFrame in install_progress_frame.cpp.

--- src/ui/frames/install_progress_frame.cpp
+++ src/ui/frames/install_progress_frame.cpp
@@ -49,223 +49,283 @@ const int kTooltipLabelMargin = 2;
 const int kTooltipFrameWidth = kProgressBarWidth + kTooltipWidth;
 
 const int kRetainingInterval = 3000;
-
 const int kSimulationTimerInterval = 3000;
-
 const int kProgressAnimationDuration = 500;
 
 }  // namespace
 
+class InstallProgressFramePrivate : public QObject{
+    Q_OBJECT
+
+public:
+    InstallProgressFramePrivate(InstallProgressFrame* ptr);
+    ~InstallProgressFramePrivate();
+
+    void initConnections();
+    void initUI();
+
+    // Update value of progress bar to |progress| and update tooltip position.
+    void updateProgressBar(int progress);
+
+    // Handles error state
+    void onHooksErrorOccurred();
+    // Handles successful installation.
+    void onHooksFinished();
+    void onProgressUpdate(int progress);
+    void onRetainingTimerTimeout();
+    void onSimulationTimerTimeout();
+
+    InstallProgressFrame* m_ptr = nullptr;
+
+    bool failed_;
+
+    // Progress value.
+    int progress_;
+
+    HooksManager* hooks_manager_ = nullptr;
+    QThread* hooks_manager_thread_ = nullptr;
+
+    TitleLabel* title_label_ = nullptr;
+    CommentLabel* comment_label_ = nullptr;
+    InstallProgressSlideFrame* slide_frame_ = nullptr;
+    QLabel* tooltip_label_ = nullptr;
+    QProgressBar* progress_bar_ = nullptr;
+
+    QPropertyAnimation* progress_animation_ = nullptr;
+
+    QTimer* simulation_timer_ = nullptr;
+};
+
 InstallProgressFrame::InstallProgressFrame(QWidget* parent)
-    : QFrame(parent),
+    : QFrame(parent)
+      , d_private(new InstallProgressFramePrivate(this))
+{
+    this->setObjectName("install_progress_frame");
+}
+
+InstallProgressFrame::~InstallProgressFrame()
+{}
+
+bool InstallProgressFrame::failed() const
+{
+    Q_D(const InstallProgressFrame);
+
+    return d->failed_;
+}
+
+void InstallProgressFrame::startSlide() {
+    const bool disable_slide =
+            GetSettingsBool(kInstallProgressPageDisableSlide);
+    const bool disable_animation =
+            GetSettingsBool(kInstallProgressPageDisableSlideAnimation);
+    const int duration = GetSettingsInt(kInstallProgressPageAnimationDuration);
+
+    Q_D(InstallProgressFrame);
+    d->slide_frame_->startSlide(disable_slide, disable_animation, duration);
+}
+
+void InstallProgressFrame::runHooks(bool ok) {
+    qDebug() << "runHooks()" << ok;
+
+    Q_D(InstallProgressFrame);
+
+    if (ok) {
+        // Partition operations take 5% progress.
+        d->onProgressUpdate(kBeforeChrootStartVal);
+
+        qDebug() << "emit runHooks() signal";
+        // Notify HooksManager to run hooks/ in background thread.
+        // Do not run hooks in debug mode.
+#ifdef NDEBUG
+        emit d->hooks_manager_->runHooks();
+#endif
+    } else {
+        d->onHooksErrorOccurred();
+    }
+}
+
+void InstallProgressFrame::simulate() {
+    Q_D(InstallProgressFrame);
+
+    if (!d->simulation_timer_->isActive()) {
+        this->startSlide();
+
+        // Reset progress value.
+        d->onProgressUpdate(d->progress_bar_->minimum());
+        d->simulation_timer_->start();
+    }
+}
+
+void InstallProgressFrame::changeEvent(QEvent* event) {
+    Q_D(InstallProgressFrame);
+
+    if (event->type() == QEvent::LanguageChange) {
+        d->title_label_->setText(tr("Installing"));
+        d->comment_label_->setText(
+                    tr("Here are a few cool things to look out for..."));
+    } else {
+        QFrame::changeEvent(event);
+    }
+}
+
+InstallProgressFramePrivate::InstallProgressFramePrivate(InstallProgressFrame* ptr)
+    : m_ptr(ptr),
       failed_(true),
       progress_(0),
       hooks_manager_(new HooksManager()),
       hooks_manager_thread_(new QThread(this)),
-      simulation_timer_(new QTimer(this)) {
-  this->setObjectName("install_progress_frame");
+      simulation_timer_(new QTimer(this))
+{
+    hooks_manager_->moveToThread(hooks_manager_thread_);
 
-  hooks_manager_->moveToThread(hooks_manager_thread_);
+    this->initUI();
+    this->initConnections();
 
-  this->initUI();
-  this->initConnections();
+    hooks_manager_thread_->start();
 
-  hooks_manager_thread_->start();
-
-  simulation_timer_->setSingleShot(false);
-  simulation_timer_->setInterval(kSimulationTimerInterval);
+    simulation_timer_->setSingleShot(false);
+    simulation_timer_->setInterval(kSimulationTimerInterval);
 }
 
-InstallProgressFrame::~InstallProgressFrame() {
-  QuitThread(hooks_manager_thread_);
+InstallProgressFramePrivate::~InstallProgressFramePrivate()
+{
+    QuitThread(hooks_manager_thread_);
 }
 
-void InstallProgressFrame::startSlide() {
-  const bool disable_slide =
-      GetSettingsBool(kInstallProgressPageDisableSlide);
-  const bool disable_animation =
-      GetSettingsBool(kInstallProgressPageDisableSlideAnimation);
-  const int duration = GetSettingsInt(kInstallProgressPageAnimationDuration);
-  slide_frame_->startSlide(disable_slide, disable_animation, duration);
+void InstallProgressFramePrivate::initConnections() {
+    connect(hooks_manager_, &HooksManager::errorOccurred,
+            this, &InstallProgressFramePrivate::onHooksErrorOccurred);
+    connect(hooks_manager_, &HooksManager::finished,
+            this, &InstallProgressFramePrivate::onHooksFinished);
+    connect(hooks_manager_, &HooksManager::processUpdate,
+            this, &InstallProgressFramePrivate::onProgressUpdate);
+    connect(hooks_manager_thread_, &QThread::finished,
+            hooks_manager_, &HooksManager::deleteLater);
+    connect(simulation_timer_, &QTimer::timeout,
+            this, &InstallProgressFramePrivate::onSimulationTimerTimeout);
 }
 
-void InstallProgressFrame::simulate() {
-  if (!simulation_timer_->isActive()) {
-    this->startSlide();
+void InstallProgressFramePrivate::initUI() {
+    title_label_ = new TitleLabel(tr("Installing"));
+    comment_label_ = new CommentLabel(
+                tr("Here are a few cool things to look out for..."));
+    QHBoxLayout* comment_layout = new QHBoxLayout();
+    comment_layout->setContentsMargins(0, 0, 0, 0);
+    comment_layout->setSpacing(0);
+    comment_layout->addWidget(comment_label_);
 
-    // Reset progress value.
-    this->onProgressUpdate(progress_bar_->minimum());
-    simulation_timer_->start();
-  }
-}
+    slide_frame_ = new InstallProgressSlideFrame();
 
-void InstallProgressFrame::runHooks(bool ok) {
-  qDebug() << "runHooks()" << ok;
-
-  if (ok) {
-    // Partition operations take 5% progress.
-    this->onProgressUpdate(kBeforeChrootStartVal);
-
-    qDebug() << "emit runHooks() signal";
-    // Notify HooksManager to run hooks/ in background thread.
-    // Do not run hooks in debug mode.
-#ifdef NDEBUG
-    emit hooks_manager_->runHooks();
-#endif
-  } else {
-    this->onHooksErrorOccurred();
-  }
-}
-
-void InstallProgressFrame::setProgress(int progress) {
-  progress_ = progress;
-  this->updateProgressBar(progress);
-}
-
-void InstallProgressFrame::changeEvent(QEvent* event) {
-  if (event->type() == QEvent::LanguageChange) {
-    title_label_->setText(tr("Installing"));
-    comment_label_->setText(
-        tr("Here are a few cool things to look out for..."));
-  } else {
-    QFrame::changeEvent(event);
-  }
-}
-
-void InstallProgressFrame::initConnections() {
-  connect(hooks_manager_, &HooksManager::errorOccurred,
-          this, &InstallProgressFrame::onHooksErrorOccurred);
-  connect(hooks_manager_, &HooksManager::finished,
-          this, &InstallProgressFrame::onHooksFinished);
-  connect(hooks_manager_, &HooksManager::processUpdate,
-          this, &InstallProgressFrame::onProgressUpdate);
-
-  connect(hooks_manager_thread_, &QThread::finished,
-          hooks_manager_, &HooksManager::deleteLater);
-
-  connect(simulation_timer_, &QTimer::timeout,
-          this, &InstallProgressFrame::onSimulationTimerTimeout);
-}
-
-void InstallProgressFrame::initUI() {
-  title_label_ = new TitleLabel(tr("Installing"));
-  comment_label_ = new CommentLabel(
-      tr("Here are a few cool things to look out for..."));
-  QHBoxLayout* comment_layout = new QHBoxLayout();
-  comment_layout->setContentsMargins(0, 0, 0, 0);
-  comment_layout->setSpacing(0);
-  comment_layout->addWidget(comment_label_);
-
-  slide_frame_ = new InstallProgressSlideFrame();
-
-  QFrame* tooltip_frame = new QFrame();
-  tooltip_frame->setObjectName("tooltip_frame");
-  tooltip_frame->setContentsMargins(0, 0, 0, 0);
-  tooltip_frame->setFixedSize(kTooltipFrameWidth, kTooltipHeight);
-  tooltip_label_ = new TooltipPin(tooltip_frame);
-  tooltip_label_->setFixedSize(kTooltipWidth, kTooltipHeight);
-  tooltip_label_->setAlignment(Qt::AlignHCenter);
-  tooltip_label_->setText("0%");
-  // Add left margin.
-  tooltip_label_->move(kTooltipLabelMargin, tooltip_label_->y());
-
-  // NOTE(xushaohua): QProgressBar::paintEvent() has performance issue on
-  // loongson platform, when chunk style is set. So we override paintEvent()
-  // and draw progress bar chunk by hand.
-  progress_bar_ = new RoundedProgressBar();
-  progress_bar_->setObjectName("progress_bar");
-  progress_bar_->setFixedSize(kProgressBarWidth, 8);
-  progress_bar_->setTextVisible(false);
-  // Set progress range to [0, 1000] so that progress bar can be painted
-  // more smoothly.
-  progress_bar_->setRange(0, 1000);
-  progress_bar_->setOrientation(Qt::Horizontal);
-  progress_bar_->setValue(0);
-
-  QVBoxLayout* layout = new QVBoxLayout();
-  layout->setContentsMargins(0, 0, 0, 0);
-  layout->setSpacing(0);
-  layout->addStretch();
-  layout->addWidget(title_label_, 0, Qt::AlignCenter);
-  layout->addSpacing(kMainLayoutSpacing);
-  layout->addLayout(comment_layout);
-  layout->addStretch();
-  layout->addWidget(slide_frame_, 0, Qt::AlignCenter);
-  layout->addStretch();
-  layout->addWidget(tooltip_frame, 0, Qt::AlignHCenter);
-  layout->addSpacing(5);
-  layout->addWidget(progress_bar_, 0, Qt::AlignCenter);
-  layout->addStretch();
-
-  this->setLayout(layout);
-  this->setContentsMargins(0, 0, 0, 0);
-  this->setStyleSheet(ReadFile(":/styles/install_progress_frame.css"));
-
-  progress_animation_ = new QPropertyAnimation(this, "progress", this);
-  progress_animation_->setDuration(kProgressAnimationDuration);
-  progress_animation_->setEasingCurve(QEasingCurve::InOutCubic);
-}
-
-void InstallProgressFrame::updateProgressBar(int progress) {
-  Q_ASSERT(progress_bar_->maximum() != 0);
-  progress_bar_->setValue(progress);
-  const double percentage = progress * 1.0 / progress_bar_->maximum();
-
-  // Calculate percentage of progress.
-  const int real_progress = int(floor(percentage * 100.0));
-  tooltip_label_->setText(QString("%1%").arg(real_progress));
-  // Add right margin.
-  int x = int(kProgressBarWidth * percentage - kTooltipLabelMargin);
-  if (x < kTooltipLabelMargin) {
+    QFrame* tooltip_frame = new QFrame();
+    tooltip_frame->setObjectName("tooltip_frame");
+    tooltip_frame->setContentsMargins(0, 0, 0, 0);
+    tooltip_frame->setFixedSize(kTooltipFrameWidth, kTooltipHeight);
+    tooltip_label_ = new TooltipPin(tooltip_frame);
+    tooltip_label_->setFixedSize(kTooltipWidth, kTooltipHeight);
+    tooltip_label_->setAlignment(Qt::AlignHCenter);
+    tooltip_label_->setText("0%");
     // Add left margin.
-    x = kTooltipLabelMargin;
-  }
-  const int y = tooltip_label_->y();
-  tooltip_label_->move(x, y);
+    tooltip_label_->move(kTooltipLabelMargin, tooltip_label_->y());
 
-  // Force QProgressBar to repaint.
-  this->style()->unpolish(progress_bar_);
-  this->style()->polish(progress_bar_);
-  progress_bar_->repaint();
+    // NOTE(xushaohua): QProgressBar::paintEvent() has performance issue on
+    // loongson platform, when chunk style is set. So we override paintEvent()
+    // and draw progress bar chunk by hand.
+    progress_bar_ = new RoundedProgressBar();
+    progress_bar_->setObjectName("progress_bar");
+    progress_bar_->setFixedSize(kProgressBarWidth, 8);
+    progress_bar_->setTextVisible(false);
+    // Set progress range to [0, 1000] so that progress bar can be painted
+    // more smoothly.
+    progress_bar_->setRange(0, 1000);
+    progress_bar_->setOrientation(Qt::Horizontal);
+    progress_bar_->setValue(0);
+
+    QVBoxLayout* layout = new QVBoxLayout();
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setSpacing(0);
+    layout->addStretch();
+    layout->addWidget(title_label_, 0, Qt::AlignCenter);
+    layout->addSpacing(kMainLayoutSpacing);
+    layout->addLayout(comment_layout);
+    layout->addStretch();
+    layout->addWidget(slide_frame_, 0, Qt::AlignCenter);
+    layout->addStretch();
+    layout->addWidget(tooltip_frame, 0, Qt::AlignHCenter);
+    layout->addSpacing(5);
+    layout->addWidget(progress_bar_, 0, Qt::AlignCenter);
+    layout->addStretch();
+
+    m_ptr->setLayout(layout);
+    m_ptr->setContentsMargins(0, 0, 0, 0);
+    m_ptr->setStyleSheet(ReadFile(":/styles/install_progress_frame.css"));
+
+    progress_animation_ = new QPropertyAnimation(this, "progress", this);
+    progress_animation_->setDuration(kProgressAnimationDuration);
+    progress_animation_->setEasingCurve(QEasingCurve::InOutCubic);
 }
 
-void InstallProgressFrame::onHooksErrorOccurred() {
-  failed_ = true;
-  slide_frame_->stopSlide();
-  emit this->finished();
+void InstallProgressFramePrivate::updateProgressBar(int progress) {
+    Q_ASSERT(progress_bar_->maximum() != 0);
+    progress_bar_->setValue(progress);
+    const double percentage = progress * 1.0 / progress_bar_->maximum();
+
+    // Calculate percentage of progress.
+    const int real_progress = int(floor(percentage * 100.0));
+    tooltip_label_->setText(QString("%1%").arg(real_progress));
+    // Add right margin.
+    int x = int(kProgressBarWidth * percentage - kTooltipLabelMargin);
+    if (x < kTooltipLabelMargin) {
+        // Add left margin.
+        x = kTooltipLabelMargin;
+    }
+    const int y = tooltip_label_->y();
+    tooltip_label_->move(x, y);
+
+    // Force QProgressBar to repaint.
+    m_ptr->style()->unpolish(progress_bar_);
+    m_ptr->style()->polish(progress_bar_);
+    progress_bar_->repaint();
 }
 
-void InstallProgressFrame::onHooksFinished() {
-  failed_ = false;
-
-  // Set progress value to 100 explicitly.
-  this->onProgressUpdate(100);
-
-  QTimer::singleShot(kRetainingInterval,
-                     this, &InstallProgressFrame::onRetainingTimerTimeout);
+void InstallProgressFramePrivate::onHooksErrorOccurred() {
+    failed_ = true;
+    slide_frame_->stopSlide();
+    emit m_ptr->finished();
 }
 
-void InstallProgressFrame::onProgressUpdate(int progress) {
-  // Multiple progress value by 10 to fit progress_bar_ range.
-  const int virtual_progress = progress * 10;
-  progress_animation_->setEndValue(virtual_progress);
-  progress_animation_->start();
+void InstallProgressFramePrivate::onHooksFinished() {
+    failed_ = false;
+
+    // Set progress value to 100 explicitly.
+    this->onProgressUpdate(100);
+
+    QTimer::singleShot(kRetainingInterval,
+                       this, &InstallProgressFramePrivate::onRetainingTimerTimeout);
 }
 
-void InstallProgressFrame::onRetainingTimerTimeout() {
-  slide_frame_->stopSlide();
-  emit this->finished();
+void InstallProgressFramePrivate::onProgressUpdate(int progress) {
+    // Multiple progress value by 10 to fit progress_bar_ range.
+    const int virtual_progress = progress * 10;
+    progress_animation_->setEndValue(virtual_progress);
+    progress_animation_->start();
 }
 
-void InstallProgressFrame::onSimulationTimerTimeout() {
-  // Increase 5% each time.
-  const int progress = (progress_bar_->value() + 10) * 100
-                       / progress_bar_->maximum();
-  if (progress > progress_bar_->maximum()) {
-    simulation_timer_->stop();
-  } else {
-    this->onProgressUpdate(progress);
-  }
+void InstallProgressFramePrivate::onRetainingTimerTimeout() {
+    slide_frame_->stopSlide();
+    emit m_ptr->finished();
+}
+
+void InstallProgressFramePrivate::onSimulationTimerTimeout() {
+    // Increase 5% each time.
+    const int progress = (progress_bar_->value() + 10) * 100
+            / progress_bar_->maximum();
+    if (progress > progress_bar_->maximum()) {
+        simulation_timer_->stop();
+    } else {
+        this->onProgressUpdate(progress);
+    }
 }
 
 }  // namespace installer
+
+#include "install_progress_frame.moc"
--- src/ui/frames/install_progress_frame.h
+++ src/ui/frames/install_progress_frame.h
@@ -19,92 +19,46 @@
 #define INSTALLER_UI_FRAMES_INSTALL_PROGRESS_FRAME_H
 
 #include <QFrame>
-class QLabel;
-class QProgressBar;
-class QPropertyAnimation;
-class QThread;
-class QTimer;
+#include <QScopedPointer>
 
 namespace installer {
 
-class CommentLabel;
-class HooksManager;
-class InstallProgressBar;
-class InstallProgressSlideFrame;
-class TitleLabel;
+class InstallProgressFramePrivate;
 
 // Displays when system is being installed to disk.
 // A progress bar is shown at bottom of page.
 class InstallProgressFrame : public QFrame {
-  Q_OBJECT
-  Q_PROPERTY(int progress READ progress WRITE setProgress);
+    Q_OBJECT
+//    Q_PROPERTY(int progress READ progress WRITE setProgress);
 
- public:
-  explicit InstallProgressFrame(QWidget* parent = nullptr);
-  ~InstallProgressFrame();
+public:
+    explicit InstallProgressFrame(QWidget* parent = nullptr);
+    ~InstallProgressFrame();
 
-  // Returns true is installation process failed.
-  bool failed() const { return failed_; }
+    // Returns true is installation process failed.
+    bool failed() const;
 
-  int progress() const { return progress_; }
+    // Show slide now.
+    void startSlide();
 
-  // Show slide now.
-  void startSlide();
+public slots:
+    // Run hooks when partition job is done
+    void runHooks(bool ok);
 
- public slots:
-  // Run hooks when partition job is done
-  void runHooks(bool ok);
+    // Update progress value with a QTimer object.
+    void simulate();
 
-  void setProgress(int progress);
+signals:
+    // Emitted when installation finished or failed.
+    // Call state() to check its status.
+    void finished();
 
-  // Update progress value with a QTimer object.
-  void simulate();
+protected:
+    void changeEvent(QEvent* event) override;
 
- signals:
-  // Emitted when installation finished or failed.
-  // Call state() to check its status.
-  void finished();
-
- protected:
-  void changeEvent(QEvent* event) override;
-
- private:
-  void initConnections();
-  void initUI();
-
-  // Update value of progress bar to |progress| and update tooltip position.
-  void updateProgressBar(int progress);
-
-  bool failed_;
-
-  // Progress value.
-  int progress_;
-
-  HooksManager* hooks_manager_ = nullptr;
-  QThread* hooks_manager_thread_ = nullptr;
-
-  TitleLabel* title_label_ = nullptr;
-  CommentLabel* comment_label_ = nullptr;
-  InstallProgressSlideFrame* slide_frame_ = nullptr;
-  QLabel* tooltip_label_ = nullptr;
-  QProgressBar* progress_bar_ = nullptr;
-
-  QPropertyAnimation* progress_animation_ = nullptr;
-
-  QTimer* simulation_timer_ = nullptr;
-
- private slots:
-  // Handles error state
-  void onHooksErrorOccurred();
-
-  // Handles successful installation.
-  void onHooksFinished();
-
-  void onProgressUpdate(int progress);
-
-  void onRetainingTimerTimeout();
-
-  void onSimulationTimerTimeout();
+private:
+    QScopedPointer<InstallProgressFramePrivate> d_private;
+    Q_DECLARE_PRIVATE_D(d_private, InstallProgressFrame)
 };
 
 }  // namespace installer
