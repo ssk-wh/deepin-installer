5f2cb74f3237cd3fe42f19764eeefdb926ddc5b8
程向璐 <chengxianglu_cm@deepin.com>
2019-12-07 15:04:35 +0800

    refactor(keyboard): Use QStandarditemModel replace
     old model,Change page css.

--- src/ui/frames/inner/system_info_keyboard_frame.cpp
+++ src/ui/frames/inner/system_info_keyboard_frame.cpp
@@ -22,6 +22,12 @@
 #include <QLineEdit>
 #include <QVBoxLayout>
 #include <QScrollBar>
+#include <QStandardItemModel>
+#include <QCollator>
+#include <algorithm>
+
+#include <DListView>
+#include <DStandardItem>
 
 #include "base/file_util.h"
 #include "service/settings_manager.h"
@@ -34,6 +40,8 @@
 #include "ui/widgets/nav_button.h"
 #include "ui/widgets/title_label.h"
 #include "ui/widgets/comment_label.h"
+
+DWIDGET_USE_NAMESPACE
 
 namespace installer {
 
@@ -71,20 +79,58 @@ private:
     void onVariantViewSelected(const QModelIndex& current,
                                const QModelIndex& previous);
 
+    // Get index of layout with |name|.
+    // Result might be invalid.
+    QModelIndex getLayoutByName(const QString& name);
+
+    // Get layout name at |index|.
+    QString getLayoutName(const QModelIndex& index);
+
+    // Read xkb layout list with |locale|.
+    void initLayout(const QString& locale);
+
+    // Get variant list at |index|.
+    XKbLayoutVariantList getVariantList(const QModelIndex& index);
+
+    // Get layout description at |index|.
+    QString getLayoutDescription(const QModelIndex& index);
+
+    QModelIndex getVariantIndexByName(const QString& name);
+
+    // Get layout variant name at |index|.
+    QString getVariantName(const QModelIndex& index);
+
+    // Set variant list and emit dataChanged() signal.
+    void setVariantList(const XKbLayoutVariantList& variant_list,
+                        const QString& locale);
+
+    // Get description at |index|.
+    QString getVariantDescription(const QModelIndex& index);
+
+
     TitleLabel* m_titleLabel = new TitleLabel("");
     QLabel* m_guideLabel = new CommentLabel;
-    FramelessListView* m_layoutView = new FramelessListView;
-    KeyboardLayoutModel* m_layoutModel = new KeyboardLayoutModel;
-    FramelessListView* m_variantView = new FramelessListView;
-    KeyboardLayoutVariantModel* m_variantModel = new KeyboardLayoutVariantModel;
+    QStandardItemModel* m_layoutModel = new QStandardItemModel;
+    QStandardItemModel* m_variantModel = new QStandardItemModel;
     QLineEdit* m_testEdit = new QLineEdit;
-    NavButton* m_backButton = new NavButton;
     QString m_currentLocale;
+    NavButton* m_backButton = new NavButton;
+    XkbConfig xkb_config_;
+
+    DListView* m_layoutView = new DListView;
+    DListView* m_variantView = new DListView;
+    DStandardItem* m_lastItem = nullptr;
+    DStandardItem* m_lastItemVar = nullptr;
+
+    // Keyboard layout list sorted by description.
+    XkbLayoutList layout_list_;
+    XKbLayoutVariantList variant_list_;
+    XkbModelList model_list;
 };
 
 SystemInfoKeyboardFrame::SystemInfoKeyboardFrame(QWidget* parent)
     : QFrame(parent)
-    ,d_private(new SystemInfoKeyboardFramePrivate(this))
+    , d_private(new SystemInfoKeyboardFramePrivate(this))
 {
     this->setObjectName("system_info_keyboard_frame");
 
@@ -99,6 +145,133 @@ SystemInfoKeyboardFrame::~SystemInfoKeyboardFrame()
 
 }
 
+QModelIndex SystemInfoKeyboardFramePrivate::getLayoutByName(const QString& name) {
+  for (int row = 0; row < layout_list_.length(); ++row) {
+    if (layout_list_.at(row).name == name) {
+      return m_layoutModel->index(row,0);
+    }
+  }
+  return QModelIndex();
+}
+
+QString SystemInfoKeyboardFramePrivate::getLayoutName(const QModelIndex& index) {
+  if (index.isValid()) {
+    return layout_list_.at(index.row()).name;
+  } else {
+    return QString();
+  }
+}
+
+void SystemInfoKeyboardFramePrivate::initLayout(const QString& locale) {
+  // Load xkb layout based on current locale.
+  // Locale environment is setup in SelectLanguageFrame.
+  xkb_config_ = GetXkbConfig(locale);
+  layout_list_ = xkb_config_.layout_list;
+
+  // Append layout to its variant list.
+  for (XkbLayout& layout : layout_list_) {
+    XkbLayoutVariant variant;
+    variant.name = layout.name;
+    variant.description = layout.description;
+    variant.short_description = layout.short_description;
+    variant.language_list = layout.language_list;
+    layout.variant_list.prepend(variant);
+  }
+
+  // Sort layout list by description.
+  // Perform localized comparison.
+  const QLocale curr_locale(locale);
+  QCollator collator(curr_locale);
+  collator.setCaseSensitivity(Qt::CaseInsensitive);
+  std::sort(layout_list_.begin(), layout_list_.end(),
+            [&](const XkbLayout& a, const XkbLayout& b) -> bool {
+              return collator.compare(a.description, b.description) < 0;
+            });
+
+  for(auto it = layout_list_.cbegin(); it!=layout_list_.cend(); ++it) {
+      DStandardItem *item = new DStandardItem((*it).description);
+      m_layoutModel->appendRow(item);
+  }
+}
+
+XKbLayoutVariantList SystemInfoKeyboardFramePrivate::getVariantList(
+    const QModelIndex& index) {
+  if (index.isValid()) {
+    return layout_list_.at(index.row()).variant_list;
+  } else {
+    return XKbLayoutVariantList();
+  }
+}
+
+QString SystemInfoKeyboardFramePrivate::getLayoutDescription(
+    const QModelIndex& index) {
+  if (index.isValid()) {
+    return layout_list_.at(index.row()).description;
+  } else {
+    return QString();
+  }
+}
+
+QModelIndex SystemInfoKeyboardFramePrivate::getVariantIndexByName(const QString& name)
+{
+    for (int row = 0; row < variant_list_.length(); row++) {
+        if (variant_list_.at(row).name == name) {
+            return m_variantModel->index(row,0);
+        }
+    }
+    return QModelIndex();
+}
+
+void SystemInfoKeyboardFramePrivate::setVariantList(
+    const XKbLayoutVariantList& variant_list, const QString& locale) {
+  Q_UNUSED(locale);
+
+  variant_list_ = variant_list;
+
+  if (variant_list_.isEmpty()) {
+    qCritical() << "VariantList is empty! We shall never reach here!";
+    return;
+  }
+
+  // Sort variant list by description.
+  const QLocale curr_locale(locale);
+  QCollator collator(curr_locale);
+  collator.setCaseSensitivity(Qt::CaseInsensitive);
+
+  // Sorting variant list by description, skipping the first item.
+  std::sort(variant_list_.begin() + 1, variant_list_.end(),
+            [&](const XkbLayoutVariant& a, const XkbLayoutVariant& b) -> bool {
+              return collator.compare(a.description, b.description) < 0;
+            });
+
+  // Call this method resulting in emitting dataChanged() signal.
+
+  for(auto it = variant_list_.cbegin(); it!=variant_list_.cend(); ++it) {
+      DStandardItem* item = new DStandardItem((*it).description);
+      m_variantModel->appendRow(item);
+  }
+}
+
+QString SystemInfoKeyboardFramePrivate::getVariantName(
+    const QModelIndex& index) {
+  if (index.isValid()) {
+    return variant_list_.at(index.row()).name;
+  } else {
+    return QString();
+  }
+}
+
+QString SystemInfoKeyboardFramePrivate::getVariantDescription(
+    const QModelIndex& index) {
+  if (index.isValid()) {
+    return variant_list_.at(index.row()).description;
+  } else {
+    return QString();
+  }
+}
+
+
+
 void SystemInfoKeyboardFramePrivate::updateTs() {
     m_titleLabel->setText(tr("Select keyboard layout"));
     m_testEdit->setPlaceholderText(tr("Test here"));
@@ -111,7 +284,7 @@ void SystemInfoKeyboardFrame::readConf() {
 
     // Load xkb config first.
     d->m_currentLocale = ReadLocale();
-    d->m_layoutModel->initLayout(d->m_currentLocale);
+    d->initLayout(d->m_currentLocale);
 
     const QString kb_layout = GetSettingsString("DI_LAYOUT");
     const QString kb_variant = GetSettingsString("DI_LAYOUT_VARIANT");
@@ -125,11 +298,11 @@ void SystemInfoKeyboardFrame::readConf() {
         return;
     }
 
-    const QModelIndex index = d->m_layoutModel->getLayoutByName(layout);
+    const QModelIndex index = d->getLayoutByName(layout);
     if (index.isValid()) {
         // Select default layout.l
         d->m_layoutView->setCurrentIndex(index);
-        const QModelIndex variant_index = d->m_variantModel->getVariantIndexByName(variant);
+        const QModelIndex variant_index = d->getVariantIndexByName(variant);
         if (variant_index.isValid()) {
             d->m_variantView->setCurrentIndex(variant_index);
         }
@@ -139,15 +312,16 @@ void SystemInfoKeyboardFrame::readConf() {
     } else {
         qWarning() << "Invalid default keyboard layout:" << layout;
     }
+
 }
 
 void SystemInfoKeyboardFrame::writeConf() {
     Q_D(SystemInfoKeyboardFrame);
 
     const QModelIndex layout_index = d->m_layoutView->currentIndex();
-    const QString layout = d->m_layoutModel->getLayoutName(layout_index);
+    const QString layout = d->getLayoutName(layout_index);
     const QModelIndex variant_index = d->m_variantView->currentIndex();
-    const QString variant = d->m_variantModel->getVariantName(variant_index);
+    const QString variant = d->getVariantName(variant_index);
 
     // Model name of keyboard is empty. Variant name might be empty.
     // The first row in variant list is the default layout.
@@ -165,11 +339,11 @@ void SystemInfoKeyboardFrame::changeEvent(QEvent* event) {
         d->updateTs();
 
         const QString& locale = ReadLocale();
-        d->m_layoutModel->initLayout(locale);
+        d->initLayout(locale);
 
         int index = locale.indexOf('_');
         if (index >= 0){
-            const QModelIndex modelIndex = d->m_layoutModel->getLayoutByName(locale.mid(index + 1).toLower());
+            const QModelIndex modelIndex = d->getLayoutByName(locale.mid(index + 1).toLower());
             if (modelIndex.isValid()) {
                 d->m_layoutView->setCurrentIndex(modelIndex);
             }
@@ -204,6 +378,13 @@ void SystemInfoKeyboardFramePrivate::initUI() {
     m_layoutView->setContextMenuPolicy(Qt::NoContextMenu);
     m_layoutView->horizontalScrollBar()->setContextMenuPolicy(Qt::NoContextMenu);
     m_layoutView->verticalScrollBar()->setContextMenuPolicy(Qt::NoContextMenu);
+    m_layoutView->setSizeAdjustPolicy(QAbstractScrollArea::AdjustToContents);
+    m_layoutView->setEditTriggers(QListView::NoEditTriggers);
+    m_layoutView->setIconSize(QSize(32, 32));
+    m_layoutView->setResizeMode(QListView::Adjust);
+    m_layoutView->setMovement(QListView::Static);
+    m_layoutView->setSelectionMode(QListView::NoSelection);
+    m_layoutView->setFrameShape(QFrame::NoFrame);
 
     m_variantView->setObjectName("variant_view");
     m_variantView->setFixedWidth(499);
@@ -212,6 +393,13 @@ void SystemInfoKeyboardFramePrivate::initUI() {
     m_variantView->setContextMenuPolicy(Qt::NoContextMenu);
     m_variantView->horizontalScrollBar()->setContextMenuPolicy(Qt::NoContextMenu);
     m_variantView->verticalScrollBar()->setContextMenuPolicy(Qt::NoContextMenu);
+    m_variantView->setSizeAdjustPolicy(QAbstractScrollArea::AdjustToContents);
+    m_variantView->setEditTriggers(QListView::NoEditTriggers);
+    m_variantView->setIconSize(QSize(32, 32));
+    m_variantView->setResizeMode(QListView::Adjust);
+    m_variantView->setMovement(QListView::Static);
+    m_variantView->setSelectionMode(QListView::NoSelection);
+    m_variantView->setFrameShape(QFrame::NoFrame);
 
     QHBoxLayout* keyboard_layout = new QHBoxLayout();
     keyboard_layout->setContentsMargins(0, 0, 0, 0);
@@ -267,17 +455,25 @@ void SystemInfoKeyboardFramePrivate::onLayoutViewSelectionChanged(
     Q_UNUSED(previous);
 
     m_testEdit->clear();
+    m_variantModel->clear();
+
+    DStandardItem* item = dynamic_cast<DStandardItem* >(m_layoutModel->item(current.row()));
+    item->setCheckState(Qt::Checked);
+
+    if (m_lastItem) {
+        m_lastItem->setCheckState(Qt::Unchecked);
+    }
+
+    m_lastItem = item;
 
     // Update variant list.
-    m_variantModel->setVariantList(m_layoutModel->getVariantList(current),
-                                   m_currentLocale);
+    setVariantList(getVariantList(current),  m_currentLocale);
 
     // Scroll to top of variant view.
     m_variantView->scrollToTop();
-
     if (m_layoutView->selectionModel()->selectedIndexes().size() > 0)  {
         // Select the default layout variant.
-        m_variantView->setCurrentIndex(m_variantModel->index(0));
+        m_variantView->setCurrentIndex(m_variantModel->index(0,0));
     }
 }
 
@@ -290,19 +486,30 @@ void SystemInfoKeyboardFramePrivate::onVariantViewSelected(
     // Clear content of m_testEdit when new layout variant is selected.
     m_testEdit->clear();
 
+    DStandardItem* item = dynamic_cast<DStandardItem* >(m_variantModel->item(current.row()));
+    item->setCheckState(Qt::Checked);
+
+    if (m_lastItemVar) {
+        m_lastItemVar->setCheckState(Qt::Unchecked);
+    }
+
+    m_lastItemVar = item;
+
+
+
     const QModelIndex layout_index = m_layoutView->currentIndex();
-    const QString layout = m_layoutModel->getLayoutName(layout_index);
-    const QString variant = m_variantModel->getVariantName(current);
+    const QString layout = getLayoutName(layout_index);
+    const QString variant = getVariantName(current);
     QString description;
 
     // The first row in variant list is the default layout.
     if (current.row() == 0) {
-        description = m_layoutModel->getLayoutDescription(layout_index);
+        description = getLayoutDescription(layout_index);
         if (!SetXkbLayout(layout)) {
             qWarning() << "SetXkbLayout() failed!" << layout;
         }
     } else {
-        description = m_variantModel->getVariantDescription(current);
+        description = getVariantDescription(current);
         if (!SetXkbLayout(layout, variant)) {
             qWarning() << "SetXkbLayout() failed!" << layout << variant;
         }
