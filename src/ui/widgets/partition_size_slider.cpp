491317b66cfbfd1c362695c106ebd02e633ae3ac
chenxiong <chenxiong_cm@deepin.com>
2020-04-19 21:51:49 +0800

    feat: partition size in MB unit
    partition size in MB unit in newPartitionFrame.

--- src/ui/widgets/partition_size_slider.cpp
+++ src/ui/widgets/partition_size_slider.cpp
@@ -44,7 +44,7 @@ qint64 PartitionSizeSlider::value() {
   if (slider_->value() == slider_->maximum()) {
     return maximum_size_;
   } else {
-    return slider_->value() * kGibiByte;
+    return slider_->value() * kMebiByte;
   }
 }
 
@@ -67,7 +67,7 @@ void PartitionSizeSlider::setMaximum(qint64 maximum_size) {
   Q_ASSERT(maximum_size >= 0);
   maximum_size_ = maximum_size;
   // First convert bytes to mebibytes.
-  const int mebi_size = static_cast<int>(maximum_size / kGibiByte);
+  const int mebi_size = static_cast<int>(maximum_size / kMebiByte);
   slider_->setMaximum(mebi_size);
   slider_->setValue(mebi_size);
 }
@@ -78,7 +78,7 @@ void PartitionSizeSlider::setMinimum(qint64 minimum_size) {
   const qint64 min_size = qMin(minimum_size, maximum_size_);
   minimum_size_ = min_size;
   // Convert bytes to mebibytes.
-  const int mebi_size = static_cast<int>(min_size / kGibiByte);
+  const int mebi_size = static_cast<int>(min_size / kMebiByte);
   slider_->setMinimum(mebi_size);
 }
 
@@ -88,7 +88,7 @@ void PartitionSizeSlider::setValue(qint64 size) {
   qint64 real_size = qMin(size, maximum_size_);
   real_size = qMax(size, minimum_size_);
   // Convert to mebibytes.
-  const int mebi_size = static_cast<int>(real_size / kGibiByte);
+  const int mebi_size = static_cast<int>(real_size / kMebiByte);
   slider_->setValue(mebi_size);
   editor_->setText(QString("%1").arg(mebi_size));
 }
@@ -112,7 +112,7 @@ void PartitionSizeSlider::initUI() {
   // Disable context menu.
   editor_->setContextMenuPolicy(Qt::NoContextMenu);
 
-  QLabel* size_label = new QLabel("Gb");
+  QLabel* size_label = new QLabel("MB");
   size_label->setObjectName("size_label");
 
   QHBoxLayout* layout = new QHBoxLayout();
@@ -132,7 +132,7 @@ void PartitionSizeSlider::onEditorTextChanged(const QString& text) {
   bool ok;
   int value = text.toInt(&ok);
   if (ok) {
-    const int tmp = static_cast<int>(maximum_size_ / kGibiByte);
+    const int tmp = static_cast<int>(maximum_size_ / kMebiByte);
     if(value > tmp){
       value = tmp;
       editor_->setText(QString::number(value));
@@ -144,7 +144,7 @@ void PartitionSizeSlider::onEditorTextChanged(const QString& text) {
     slider_->blockSignals(false);
 
     // Also emit valueChanged() signal.
-    emit this->valueChanged(value * kGibiByte);
+    emit this->valueChanged(value * kMebiByte);
   }
 }
 
@@ -152,7 +152,7 @@ void PartitionSizeSlider::onSliderValueChanged(int value) {
   editor_->setText(QString::number(value));
 
   // Emit valueChanged() signal.
-  emit this->valueChanged(value * kGibiByte);
+  emit this->valueChanged(value * kMebiByte);
 }
 
 }  // namespace installer
