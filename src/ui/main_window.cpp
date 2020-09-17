c0c308c24f684928cc24e5c090a437935e2ceff0
郭纳福 <guonafu@deepin.com>
2020-09-17 16:51:21 +0800

    Revert "fix: Incomplete display of rounded border"
    This reverts commit c0ea87f7579905681ee4127dd4add39ea5e2c574

--- src/ui/main_window.cpp
+++ src/ui/main_window.cpp
@@ -94,8 +94,7 @@ MainWindow::MainWindow(QWidget* parent)
     this->initPages();
     this->registerShortcut();
     this->initConnections();
-    DTitlebar* titleBar = titlebar();
-    titleBar->installEventFilter(this);
+
     setWindowFlags(windowFlags() & ~Qt::WindowCloseButtonHint & ~Qt::WindowMinMaxButtonsHint);
 
     titlebar()->setMenuVisible(false);
@@ -353,20 +352,6 @@ void MainWindow::resizeEvent(QResizeEvent *event)
     }
 
     DMainWindow::resizeEvent(event);
-}
-
-bool MainWindow::eventFilter(QObject *target, QEvent *event)
-{
-    if (target == titlebar()) {
-        if(event->type() == QEvent::MouseButtonPress)
-        {
-            QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
-            if (mouseEvent->button() == Qt::MouseButton::RightButton) {
-                return true;
-            }
-        }
-     }
-     return DMainWindow::eventFilter(target, event);
 }
 
 void MainWindow::onCloseEvent()
