a88d021c28c6538a565599d33207eb161b0b3b05
dutianxue <dutianxue@uniontech.com>
2021-02-02 16:35:48 +0800

    feat: Complete data burying & user experience plan cancellation
    完成数据埋点&用户体验计划取消任务
    
    Log: 优化数据埋点根据是否勾选体验计划进行
    
    Task: https://pms.uniontech.com/zentao/task-view-58496.html
    Change-Id: I421609816d70562627fce9a2a43e2398dc028f08

--- hooks/doinstallrecord_first_boot.sh
+++ hooks/doinstallrecord_first_boot.sh
@@ -4,12 +4,18 @@ set -x
 
 . ./basic_utils.sh
 
-DI_LOCALE=$(installer_get "DI_LOCALE")
-DI_TIMEZONE=$(installer_get "DI_TIMEZONE")
+DI_USER_EXPERIENCE=$(installer_get "DI_USER_EXPERIENCE")
 
-deepin-installer-settings set "/etc/deepin/deepin-user-experience" "ExperiencePlan" "LanguageSelected" ${DI_LOCALE}
-deepin-installer-settings set "/etc/deepin/deepin-user-experience" "ExperiencePlan" "TimeZoneSelected" ${DI_TIMEZONE}
+# if not use experience then return 
+if [ x${DI_USER_EXPERIENCE} == "xtrue" ]; then
+   DI_LOCALE=$(installer_get "DI_LOCALE")
+   DI_TIMEZONE=$(installer_get "DI_TIMEZONE")
 
-sync
+   deepin-installer-simpleini set "/etc/deepin/deepin-user-experience" "ExperiencePlan" "LanguageSelected" ${DI_LOCALE}
+   deepin-installer-simpleini set "/etc/deepin/deepin-user-experience" "ExperiencePlan" "TimeZoneSelected" ${DI_TIMEZONE}
+   sync
+else
+   sudo rm /etc/deepin/deepin-user-experience
+fi
 
 return 0
