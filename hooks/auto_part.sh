080d414b0e5a70fb3e3ed054a0098cdbf5e535db
zhangdongdong <zhangdongdong@uniontech.com>
2021-09-28 16:17:52 +0800

    feat: Resolve problems caused by code merge conflicts
    解决代码合并冲突时导致的问题
    
    Log: 在解决代码冲突时，多删除了一个字段导致问题
    
    Task: https://pms.uniontech.com/zentao/task-view-86846.html
    Change-Id: If6b9ff811613965598316fc861befc69eb8b3f97

--- hooks/auto_part.sh
+++ hooks/auto_part.sh
@@ -528,7 +528,7 @@ sava_data() {
   fi
 
   # 寻找EFI
-  local part_path="$(lsblk -lf $ROOT_DISK -o NAME,FSTYPE | grep -E "EFI" | awk '{print $1}')"
+  local part_path="$(lsblk -lf $ROOT_DISK -o NAME,LABEL | grep -E "EFI" | awk '{print $1}')"
   if [ -n $part_path ]; then
     installer_set "DI_BOOTLOADER" "/dev/$part_path"
   fi
